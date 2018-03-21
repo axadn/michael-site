1429f37acb1b990082037440a1b9bf91c49c8429
Adan Marrufo <adanmarrufo@gmail.com>
2018-03-21 15:16:09 -0700

    add dependent destroy to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     validates :title, :category, :unit_price, presence: true
     validates :title, uniqueness: true
+    has_many :order_items, dependent: :destroy
 end
