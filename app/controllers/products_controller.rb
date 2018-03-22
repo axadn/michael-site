e17fe6a8fae273cf96417b39864437d67bd9577f
Adan Marrufo <adanmarrufo@gmail.com>
2018-03-21 23:12:34 -0700

    add update to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,20 @@ class ProductsController < ApplicationController
         end
     end
 
+    def update
+        @product = Product.find_by(id: params[:id])
+        if @product
+            @product.attribs = product_params
+            if @product.save
+                render :show
+            else
+                render json: @product.errors.messages, status: 422
+            end
+        else
+            render json: {general: "no such product"}, status: 404
+        end
+    end
+
     def destroy
         @product = Product.find(params[:id])
         if @product 
@@ -39,7 +53,7 @@ class ProductsController < ApplicationController
     end
 
     def require_admin
-        @user = Session.find_by(token: session[:session_token])
+        @user = Session.find_by(token: session[:session_token]).user
         render json: {general: 'not and admin'}, status: 403 unless @user && @user.is_admin? 
     end
 end
